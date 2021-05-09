293b8d44affaf550ce589803c2980490d2f00b45
Alexsander Uglin <uglin76@mail.ru>
2021-05-09 23:17:42 +0300

    refactoring

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     raw_contact_list = data.get_raw(data_source)
     contact_list_with_doubles = parsing.raw_data(raw_contact_list)
     pure_contact_list = parsing.make_pure_contact_list(contact_list_with_doubles)
-    data.save_pure_data(pure_contact_list)
+    data.save_pure(pure_contact_list)
