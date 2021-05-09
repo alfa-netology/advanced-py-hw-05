3f3c8d281dc03361cb296d5812223570303d047f
Alexsander Uglin <uglin76@mail.ru>
2021-05-09 23:16:56 +0300

    refactoring

--- application/data.py
+++ application/data.py
@@ -6,7 +6,7 @@ def get_raw(source):
         result = list(rows)
     return result
 
-def save_pure_data(data):
+def save_pure(data):
     with open("data/phonebook_pure.csv", "w", encoding='utf-8', newline='') as f:
         writer = csv.writer(f, delimiter=',')
         writer.writerows(data)
