484a4735e31539d60796e28d976053de7066791c
Alexsander Uglin <uglin76@mail.ru>
2021-05-10 03:48:08 +0300

    refactoring

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ if __name__ == '__main__':
     raw_contact_list = data.get_raw(raw_data_path)
 
     contact_list_with_doubles = parsing.process_raw_data(raw_contact_list)
-
     pure_contact_list = parsing.make_pure_contact_list(contact_list_with_doubles)
-    pure_data_path = 'data/phonebook_pure.csv'
 
+    pure_data_path = 'data/phonebook_pure.csv'
     data.save_pure(pure_contact_list, pure_data_path)
